413b679ea0dcf623c229427cb1b1990eef4bff38
Robert Andersson <kemichal@gmail.com>
2025-03-04 21:57:27 +0100

    Update to 8.4.1

--- flake.nix
+++ flake.nix
@@ -2,89 +2,91 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }@inputs: {
+  outputs =
+    { self, nixpkgs }@inputs:
+    {
 
-    packages.x86_64-linux.default =
+      packages.x86_64-linux.default =
 
-      with import nixpkgs { system = "x86_64-linux"; };
-      stdenv.mkDerivation rec {
-        pname = "pianoteq8";
-        version = "8.3.1";
+        with import nixpkgs { system = "x86_64-linux"; };
+        stdenv.mkDerivation rec {
+          pname = "pianoteq8";
+          version = "8.4.1";
 
-        icon = fetchurl {
-          name = "pianoteq_icon_128";
-          url = "https://www.pianoteq.com/images/logo/pianoteq_icon_128.png";
-          sha256 = "sha256-lO5kz2aIpJ108L9w2BHnRmq6wQP+6rF0lqifgor8xtM=";
+          icon = fetchurl {
+            name = "pianoteq_icon_128";
+            url = "https://www.pianoteq.com/images/logo/pianoteq_icon_128.png";
+            sha256 = "sha256-lO5kz2aIpJ108L9w2BHnRmq6wQP+6rF0lqifgor8xtM=";
+          };
+
+          # IMPORTANT: Use the following command to retrive the correct hash.
+          # Otherwise the file is not found in the nix store (Add it first ofc)
+          # nix hash to-sri --type sha256 `sha256sum pianoteq_linux_v841.7z`
+          src = requireFile {
+            name = "pianoteq_linux_v841.7z";
+            message = "Download the file from: https://www.modartt.com/download?file=pianoteq_linux_v841.7z and add it to the nix store manually: nix-store --add-fixed sha256 ~/Downloads/pianoteq_linux_v841.7z";
+            sha256 = "sha256-PPRSZ0qnDfWyTaQGuB0osWvIPHYMPaSB159FDrXaY0E=";
+          };
+
+          # Alternative: Downloaded manually and place in this directory
+          # src = ./pianoteq_linux_v841.7z;
+
+          desktopItems = [
+            (makeDesktopItem {
+              name = "pianoteq8";
+              desktopName = "Pianoteq 8";
+              exec = "pianoteq8";
+              icon = "pianoteq_icon_128";
+            })
+          ];
+
+          nativeBuildInputs = [
+            p7zip
+            copyDesktopItems
+          ];
+
+          libPath = lib.makeLibraryPath [
+            alsa-lib
+            freetype
+            xorg.libX11
+            xorg.libXext
+            stdenv.cc.cc.lib
+            libjack2
+            lv2
+            libGL
+          ];
+
+          unpackCmd = "7z x ${src}";
+
+          # `runHook postInstall` is mandatory otherwise postInstall won't run
+          installPhase = ''
+            install -Dm 755 x86-64bit/Pianoteq\ 8 $out/bin/pianoteq8
+            install -Dm 755 x86-64bit/Pianoteq\ 8.lv2/Pianoteq_8.so \
+                            $out/lib/lv2/Pianoteq\ 8.lv2/Pianoteq_8.so
+            patchelf --set-interpreter "$(< $NIX_CC/nix-support/dynamic-linker)" \
+                     --set-rpath $libPath "$out/bin/pianoteq8"
+            cd x86-64bit/Pianoteq\ 8.lv2/
+            for i in *.ttl; do
+                install -D "$i" "$out/lib/lv2/Pianoteq 8.lv2/$i"
+            done
+            runHook postInstall
+          '';
+
+          # This also works instead of the following
+          # makeWrapper $out/bin/pianoteq8 $out/bin/pianoteq8_wrapped --prefix LD_LIBRARY_PATH : "$libPath"
+          fixupPhase = '':'';
+
+          # Runs copyDesktopItems hook.
+          # Alternatively call copyDesktopItems manually in installPhase/fixupPhase
+          postInstall = ''
+            install -Dm 444 ${icon} $out/share/icons/hicolor/128x128/apps/pianoteq_icon_128.png
+          '';
+
+          meta = {
+            homepage = "https://www.modartt.com/";
+            description = "Pianoteq is a virtual instrument which in contrast to other virtual instruments is physically modelled and thus can simulate the playability and complex behaviour of real acoustic instruments. Because there are no samples, the file size is just a tiny fraction of that required by other virtual instruments.";
+            platforms = lib.platforms.linux;
+          };
         };
-
-        # IMPORTANT: Use the following command to retrive the correct hash.
-        # Otherwise the file is not found in the nix store (Add it first ofc)
-        # nix hash to-sri --type sha256 `sha256sum pianoteq_linux_v831.7z`
-        src = requireFile {
-          name = "pianoteq_linux_v831.7z";
-          message = "Download the file from: https://www.modartt.com/download?file=pianoteq_linux_v831.7z and add it to the nix store manually: nix-store --add-fixed sha256 ~/Downloads/pianoteq_linux_v831.7z";
-          sha256 = "sha256-iXAYnTyc60USXJy5xPs9o0sKvmUyNNx1FFlLMdTY/LE=";
-        };
-
-        # Alternative: Downloaded manually and place in this directory
-        # src = ./pianoteq_linux_v831.7z;
-
-        desktopItems = [
-          (makeDesktopItem {
-            name = "pianoteq8";
-            desktopName = "Pianoteq 8";
-            exec = "pianoteq8";
-            icon = "pianoteq_icon_128";
-          })
-        ];
-
-        nativeBuildInputs = [
-          p7zip
-          copyDesktopItems
-        ];
-
-        libPath = lib.makeLibraryPath [
-          alsa-lib
-          freetype
-          xorg.libX11
-          xorg.libXext
-          stdenv.cc.cc.lib
-          libjack2
-          lv2
-          libGL
-        ];
-
-        unpackCmd = "7z x ${src}";
-
-        # `runHook postInstall` is mandatory otherwise postInstall won't run
-        installPhase = ''
-          install -Dm 755 x86-64bit/Pianoteq\ 8 $out/bin/pianoteq8
-          install -Dm 755 x86-64bit/Pianoteq\ 8.lv2/Pianoteq_8.so \
-                          $out/lib/lv2/Pianoteq\ 8.lv2/Pianoteq_8.so
-          patchelf --set-interpreter "$(< $NIX_CC/nix-support/dynamic-linker)" \
-                   --set-rpath $libPath "$out/bin/pianoteq8"
-          cd x86-64bit/Pianoteq\ 8.lv2/
-          for i in *.ttl; do
-              install -D "$i" "$out/lib/lv2/Pianoteq 8.lv2/$i"
-          done
-          runHook postInstall
-        '';
-
-        # This also works instead of the following
-        # makeWrapper $out/bin/pianoteq8 $out/bin/pianoteq8_wrapped --prefix LD_LIBRARY_PATH : "$libPath"
-        fixupPhase = '':'';
-
-        # Runs copyDesktopItems hook.
-        # Alternatively call copyDesktopItems manually in installPhase/fixupPhase
-        postInstall = ''
-          install -Dm 444 ${icon} $out/share/icons/hicolor/128x128/apps/pianoteq_icon_128.png
-        '';
-
-        meta = {
-          homepage = "https://www.modartt.com/";
-          description = "Pianoteq is a virtual instrument which in contrast to other virtual instruments is physically modelled and thus can simulate the playability and complex behaviour of real acoustic instruments. Because there are no samples, the file size is just a tiny fraction of that required by other virtual instruments.";
-          platforms = lib.platforms.linux;
-        };
-      };
-  };
+    };
 }
